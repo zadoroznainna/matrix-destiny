2cb404f2339a4a96f415ec4d1f10127733052957
Leksster <leksster@gmail.com>
2023-08-15 14:54:25 +0300

    :boom:

---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+	import Counter from './Counter.svelte';
+	import Matrix from './Matrix.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	// import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+	import { FloatingLabelInput, Helper } from 'flowbite-svelte';
+
+	import { Datepicker } from 'flowbite-svelte';
+
+	// import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<Datepicker datepickerFormat="dd/mm/yyyy" datepickerTitle="Дата народження" $$restProps="dasd" />
+	<div id="exampleWrapper" class="grid gap-6 items-end w-full md:grid-cols-3">
+		<FloatingLabelInput
+			style="filled"
+			id="floating_filled"
+			name="floating_filled"
+			type="text"
+			label="Floating filled"
+		/>
+		<FloatingLabelInput
+			style="outlined"
+			id="floating_outlined"
+			name="floating_outlined"
+			type="text"
+			label="Floating outlined"
+		/>
+		<FloatingLabelInput
+			id="floating_standard"
+			name="floating_standard"
+			type="text"
+			label="Floating standard"
+		/>
+	</div>
+
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2 class="text-6xl">
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+	<Matrix />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
