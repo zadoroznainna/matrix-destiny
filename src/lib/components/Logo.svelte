5defcbb664e9231dbaeb026fe9747162e7b392f4
leksster <leksster@gmail.com>
2024-09-05 14:21:39 +0300

    Adjust header for mobile

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,63 @@
+<script>
+	export let rectClass = 'stroke-slate-600';
+	export let letterClass = 'fill-slate-600';
+	export let svgClass = 'w-14 h-14';
+</script>
+
+<svg class={svgClass} viewBox="0 0 265 265" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M128.441 66.9344C129.929 64.0362 134.071 64.0362 135.559 66.9344L142.866 81.1737C144.232 83.8355 142.3 87 139.308 87H124.692C121.7 87 119.768 83.8355 121.134 81.1737L128.441 66.9344Z"
+		class={letterClass}
+	/>
+	<path
+		d="M142.337 190H114.639V189.06C114.639 184.753 116.444 182.443 120.055 182.13L123.625 181.895V117.877H123.502L114.762 117.172C114.516 116.702 114.557 115.645 114.885 114.001C115.624 111.025 117.429 109.537 120.301 109.537H148V110.477C148 114.784 146.194 117.094 142.583 117.407L139.013 117.642V181.66H139.136L147.877 182.365C148.369 187.455 146.523 190 142.337 190Z"
+		class={letterClass}
+	/>
+	<rect x="40.5" y="40.5" width="184" height="184" rx="30.5" class={rectClass} stroke-width="3" />
+	<rect
+		x="132.229"
+		y="2.12132"
+		width="184"
+		height="184"
+		rx="30.5"
+		transform="rotate(45 132.229 2.12132)"
+		class={rectClass}
+		stroke-width="3"
+	/>
+	<rect
+		x="66.4649"
+		y="18.3272"
+		width="186"
+		height="186"
+		rx="31.5"
+		transform="rotate(15 66.4649 18.3272)"
+		class={rectClass}
+	/>
+	<rect
+		x="18.3253"
+		y="66.4668"
+		width="186"
+		height="186"
+		rx="31.5"
+		transform="rotate(-15 18.3253 66.4668)"
+		class={rectClass}
+	/>
+	<rect
+		x="5.18692"
+		y="98.1869"
+		width="186"
+		height="186"
+		rx="31.5"
+		transform="rotate(-30 5.18692 98.1869)"
+		class={rectClass}
+	/>
+	<rect
+		x="5.18887"
+		y="166.27"
+		width="186"
+		height="186"
+		rx="31.5"
+		transform="rotate(-60 5.18887 166.27)"
+		class={rectClass}
+	/>
+</svg>
